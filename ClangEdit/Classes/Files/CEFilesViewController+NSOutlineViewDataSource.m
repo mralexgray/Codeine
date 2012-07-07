0096add629af86c462644fa79271785fb335a900
Jean-David Gadina <macmade@eosgarden.com>
2012-07-07 19:29:05 +0200

    Cleanup...

--- ClangEdit/Classes/Files/CEFilesViewController+NSOutlineViewDataSource.m
+++ ClangEdit/Classes/Files/CEFilesViewController+NSOutlineViewDataSource.m
@@ -9,6 +9,38 @@
 
 @implementation CEFilesViewController( NSOutlineViewDataSource )
 
+- ( NSInteger )outlineView: ( NSOutlineView * )outlineView numberOfChildrenOfItem: ( id )item
+{
+    ( void )outlineView;
+    ( void )item;
+    
+    return 0;
+}
 
+- ( BOOL )outlineView: ( NSOutlineView * )outlineView isItemExpandable: ( id )item
+{
+    ( void )outlineView;
+    ( void )item;
+    
+    return NO;
+}
+
+- ( id )outlineView: ( NSOutlineView * )outlineView child: ( NSInteger )index ofItem: ( id )item
+{
+    ( void )outlineView;
+    ( void )index;
+    ( void )item;
+    
+    return nil;
+}
+
+- ( id )outlineView: ( NSOutlineView * )outlineView objectValueForTableColumn: ( NSTableColumn * )tableColumn byItem: ( id )item
+{
+    ( void )outlineView;
+    ( void )tableColumn;
+    ( void )item;
+    
+    return nil;
+}
 
 @end
