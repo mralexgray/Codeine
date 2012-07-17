f83c102cb43ad473d129922ced6ac5a56b2773d5
Macmade <macmade@eosgarden.com>
2012-07-17 20:28:29 +0200

    Added the CEDocument class.

---
+++ ClangEdit/Classes/Helpers/CEDocument.h
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2012, Jean-David Gadina <macmade@eosgarden.com>
+ * All rights reserved
+ ******************************************************************************/
+ 
+/* $Id$ */
+
+@interface CEDocument: NSObject
+{
+@protected
+    
+    
+    
+@private
+    
+    RESERVERD_IVARS( CEDocument, 5 );
+}
+
+@end
