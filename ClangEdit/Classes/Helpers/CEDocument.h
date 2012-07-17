bc3cd1cf38db7634c0c403fbf1ff08f194b578b2
Macmade <macmade@eosgarden.com>
2012-07-17 20:41:05 +0200

    Added the base for the CEDocument class.

--- ClangEdit/Classes/Helpers/CEDocument.h
+++ ClangEdit/Classes/Helpers/CEDocument.h
@@ -5,15 +5,33 @@
  
 /* $Id$ */
 
+#import "CEFile.h"
+#import "CESourceFile.h"
+
+@class CEFile;
+
 @interface CEDocument: NSObject
 {
 @protected
     
-    
+    CEFile       * _file;
+    CESourceFile * _sourceFile;
     
 @private
     
     RESERVERD_IVARS( CEDocument, 5 );
 }
+
+@property( atomic, readonly ) CEFile        * file;
+@property( atomic, readonly ) CESourceFile  * sourceFile;
+
++ ( id )documentWithPath: ( NSString * )path;
++ ( id )documentWithURL: ( NSURL * )url;
++ ( id )documentWithLanguage: ( CESourceFileLanguage )language;
+- ( id )initWithPath: ( NSString * )path;
+- ( id )initWithURL: ( NSURL * )url;
+- ( id )initWithLanguage: ( CESourceFileLanguage )language;
+- ( void )save;
+- ( BOOL )save: ( NSError ** )error;
 
 @end
--- ClangEdit/Classes/Helpers/CEDocument.m
+++ ClangEdit/Classes/Helpers/CEDocument.m
@@ -9,6 +9,93 @@
 
 @implementation CEDocument
 
+@synthesize file        = _file;
+@synthesize sourceFile  = _sourceFile;
 
++ ( id )documentWithPath: ( NSString * )path
+{
+    return [ [ [ self alloc ] initWithPath: path ] autorelease ];
+}
+
++ ( id )documentWithURL: ( NSURL * )url
+{
+    return [ [ [ self alloc ] initWithURL: url ] autorelease ];
+}
+
++ ( id )documentWithLanguage: ( CESourceFileLanguage )language
+{
+    return [ [ [ self alloc ] initWithLanguage: language ] autorelease ];
+}
+
+- ( id )initWithPath: ( NSString * )path
+{
+    if( ( self = [ self initWithURL: [ NSURL fileURLWithPath: path ] ] ) )
+    {}
+    
+    return self;
+}
+
+- ( id )initWithURL: ( NSURL * )url
+{
+    if( ( self = [ self init ] ) )
+    {
+        _file = [ [ CEFile alloc ] initWithURL: url ];
+    
+        if( _file == nil )
+        {
+            [ self release ];
+            
+            return nil;
+        }
+        
+        _sourceFile = [ [ CESourceFile alloc ] initWithLanguage: 0 fromFile: _file.path ];
+        
+        if( _sourceFile == nil )
+        {
+            [ self release ];
+            
+            return nil;
+        }
+    }
+    
+    return self;
+}
+
+- ( id )initWithLanguage: ( CESourceFileLanguage )language
+{
+    if( ( self = [ self init ] ) )
+    {
+        _sourceFile = [ [ CESourceFile alloc ] initWithLanguage: language ];
+        
+        if( _sourceFile == nil )
+        {
+            [ self release ];
+            
+            return nil;
+        }
+    }
+    
+    return self;
+}
+
+- ( void )dealloc
+{
+    RELEASE_IVAR( _file );
+    RELEASE_IVAR( _sourceFile );
+    
+    [ super dealloc ];
+}
+
+- ( void )save
+{
+    [ self save: NULL ];
+}
+
+- ( BOOL )save: ( NSError ** )error
+{
+    ( void )error;
+    
+    return YES;
+}
 
 @end
