f932d1bc2987444f4f492b3922aa11879c0e82c9
Macmade <macmade@eosgarden.com>
2012-07-19 21:54:35 +0200

    Added a category on NSTextView.

---
+++ Codeine/Classes/Categories/NSTextView+CE.h
@@ -0,0 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2012, Jean-David Gadina <macmade@eosgarden.com>
+ * All rights reserved
+ ******************************************************************************/
+ 
+/* $Id$ */
+
+@interface NSTextView( CE )
+
+- ( NSUInteger )numberOfHardLines;
+- ( NSUInteger )numberOfSoftLines;
+
+@end
---
+++ Codeine/Classes/Categories/NSTextView+CE.m
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Copyright (c) 2012, Jean-David Gadina <macmade@eosgarden.com>
+ * All rights reserved
+ ******************************************************************************/
+ 
+/* $Id$ */
+
+#import "NSTextView+CE.h"
+
+@implementation NSTextView( CE )
+
+- ( NSUInteger )numberOfHardLines
+{
+    NSString * text;
+    NSUInteger textLength;
+    NSUInteger numberOfLines;
+    NSUInteger i;
+    
+    text          = self.textStorage.string;
+    textLength    = text.length;
+    numberOfLines = 0;
+    
+    for( i = 0; i < textLength; numberOfLines++ )
+    {
+        i = NSMaxRange( [ text lineRangeForRange: NSMakeRange( i, 0 ) ] );
+    }
+    
+    return numberOfLines;
+}
+
+- ( NSUInteger )numberOfSoftLines
+{
+    NSUInteger numberOfGlyphs;
+    NSUInteger numberOfLines;
+    NSUInteger i;
+    NSRange    range;
+    
+    numberOfGlyphs = self.layoutManager.numberOfGlyphs;
+    numberOfLines  = 0;
+    range.location = 0;
+    range.length   = 0;
+    
+    for( i = 0; i < numberOfGlyphs; numberOfLines++ )
+    {
+        [ self.layoutManager lineFragmentRectForGlyphAtIndex: i effectiveRange: &range ];
+        
+        i = NSMaxRange( range );
+    }
+    
+    return numberOfLines;
+}
+
+@end
