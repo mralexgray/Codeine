60995f3e60a3cb9538b738242864ddcc549eeef1
Macmade <macmade@eosgarden.com>
2012-07-22 19:11:13 +0200

    Added a subclass of NSTextView for the editor view.

---
+++ Codeine/Classes/Editor/CEEditorTextView.h
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) 2012, Jean-David Gadina <macmade@eosgarden.com>
+ * All rights reserved
+ ******************************************************************************/
+ 
+/* $Id$ */
+
+@interface CEEditorTextView: NSTextView
+{
+@protected
+    
+    
+    
+@private
+    
+    RESERVERD_IVARS( CEEditorTextView, 5 );
+}
+
+
+
+@end
---
+++ Codeine/Classes/Editor/CEEditorTextView.m
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2012, Jean-David Gadina <macmade@eosgarden.com>
+ * All rights reserved
+ ******************************************************************************/
+ 
+/* $Id$ */
+
+#import "CEEditorTextView.h"
+
+@implementation CEEditorTextView
+
+- ( void )drawRect: ( NSRect )rect
+{
+    [ super drawRect: rect ];
+}
+
+@end
