93b6fb3697bb7c93cd4de9ce9ecc353249a85a7a
Macmade <macmade@eosgarden.com>
2012-07-25 20:47:31 +0200

    Added a category on NSView.

---
+++ Codeine/Classes/Categories/NSView+CE.h
@@ -0,0 +1,12 @@
+/*******************************************************************************
+ * Copyright (c) 2012, Jean-David Gadina <macmade@eosgarden.com>
+ * All rights reserved
+ ******************************************************************************/
+ 
+/* $Id$ */
+
+@interface NSView( CE )
+
+- ( void )centerInSuperview;
+
+@end
