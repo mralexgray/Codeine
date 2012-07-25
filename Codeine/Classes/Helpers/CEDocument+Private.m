3b191484e9d86534dfa21766131983b3be19ba9c
Macmade <macmade@eosgarden.com>
2012-07-25 20:49:00 +0200

    Added a private category for the CEDocument class.

---
+++ Codeine/Classes/Helpers/CEDocument+Private.m
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2012, Jean-David Gadina <macmade@eosgarden.com>
+ * All rights reserved
+ ******************************************************************************/
+ 
+/* $Id$ */
+
+#import "CEDocument+Private.h"
+
+static NSUInteger       __newDocumentIndex = 0;
+static NSString * const __newDocumentName  = @"Untitled";
+
+@implementation CEDocument( Private )
+
+- ( NSString * )nameForNewDocument
+{
+    NSString * name;
+    
+    if( __newDocumentIndex == 0 )
+    {
+        name = __newDocumentName;
+    }
+    else
+    {
+        name = [ NSString stringWithFormat: @"%@-%lu", __newDocumentName, __newDocumentIndex ];
+    }
+    
+    __newDocumentIndex++;
+    
+    return name;
+}
+
+@end
