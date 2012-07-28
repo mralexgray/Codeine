5536dae16dd53ac5736185d9ec09e16018e5bf21
Macmade <macmade@eosgarden.com>
2012-07-28 02:59:50 +0200

    Added the file details view controller and XIB.

---
+++ Codeine/Classes/File Details/CEFileDetailsViewController.h
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2012, Jean-David Gadina <macmade@eosgarden.com>
+ * All rights reserved
+ ******************************************************************************/
+ 
+/* $Id$ */
+
+#import "CEViewController.h"
+
+@interface CEFileDetailsViewController: CEViewController
+{
+@protected
+    
+    
+    
+@private
+    
+    RESERVERD_IVARS( CEFileDetailsViewController, 5 );
+}
+
+
+
+@end
