19af97507f885b5606d668d8ad7d9cf2efc9e5ce
Macmade <macmade@eosgarden.com>
2012-08-20 00:26:53 +0200

    Auto-expand tabs is now working...

--- Codeine/Classes/Editor/CEEditorViewController+NSTextViewDelegate.m
+++ Codeine/Classes/Editor/CEEditorViewController+NSTextViewDelegate.m
@@ -6,9 +6,34 @@
 /* $Id$ */
 
 #import "CEEditorViewController+NSTextViewDelegate.h"
+#import "CEPreferences.h"
 
 @implementation CEEditorViewController( NSTextViewDelegate )
 
-
+- ( BOOL )textView: ( NSTextView * )textView doCommandBySelector: ( SEL )sel
+{
+    NSRange           range;
+    NSUInteger        spaces;
+    NSUInteger        i;
+    NSMutableString * text;
+    
+    if( sel == @selector( insertTab: ) && [ [ CEPreferences sharedInstance ] autoExpandTabs ] == YES )
+    {
+        range  = textView.selectedRange;
+        spaces = range.location % 4;
+        text   = [ NSMutableString string ];
+        
+        for( i = 4; i > spaces; i-- )
+        {
+            [ text appendString: @" " ];
+        }
+        
+        [ _textView insertText: text ];
+        
+        return YES;
+    }
+    
+    return NO;
+}
 
 @end
