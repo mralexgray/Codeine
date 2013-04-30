4752145870f5de3ee42f70b826e2f0c526dc6319
Macmade <macmade@eosgarden.com>
2013-04-30 20:02:30 +0200

    Changed the copyright notices. Project update.

--- Codeine/Classes/Helpers/CEDocument.m
+++ Codeine/Classes/Helpers/CEDocument.m
@@ -1,6 +1,30 @@
 /*******************************************************************************
- * Copyright (c) 2012, Jean-David Gadina <macmade@eosgarden.com>
- * All rights reserved
+ * Copyright (c) 2012, Jean-David Gadina - www.xs-labs.com
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *  -   Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *  -   Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *  -   Neither the name of 'Jean-David Gadina' nor the names of its
+ *      contributors may be used to endorse or promote products derived from
+ *      this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  ******************************************************************************/
  
 /* $Id$ */
@@ -9,12 +33,14 @@
 #import "CEDocument+Private.h"
 #import "CEFile.h"
 #import "CEPreferences.h"
+#import "CEUUID.h"
 
 @implementation CEDocument
 
 @synthesize file        = _file;
 @synthesize sourceFile  = _sourceFile;
 @synthesize name        = _name;
+@synthesize uuid        = _uuid;
 
 + ( id )documentWithPath: ( NSString * )path
 {
@@ -81,6 +107,7 @@
         }
         
         _name = [ _file.name copy ];
+        _uuid = [ CEUUID new ];
     }
     
     return self;
@@ -100,6 +127,7 @@
         }
         
         _name = [ [ self nameForNewDocument ] copy ];
+        _uuid = [ CEUUID new ];
     }
     
     return self;
@@ -110,6 +138,7 @@
     RELEASE_IVAR( _file );
     RELEASE_IVAR( _sourceFile );
     RELEASE_IVAR( _name );
+    RELEASE_IVAR( _uuid );
     
     [ super dealloc ];
 }
--- README.md
+++ README.md
@@ -1,7 +1,50 @@
-ClangEdit
-=========
+ClangKit
+========
 
-Mac OS X IDE For Clang/LLVM
----------------------------
+About
+-----
 
-[Content to come]
+ClangKit provides an Objective-C frontend to LibClang. 
+Source tokenization, diagnostics and fix-its are actually implemented.
+
+ClangKit is intended to be used as a private framework, in an application's bundle.  
+Possible applications includes:
+
+ * Source code syntax highlighting
+ * Source code tokenization
+ * Source code diagnostics
+ * Source code static analysis
+
+### Language support
+
+The project actually supports parsing C, C++, Objective-C and Objective-C++ source code.
+
+### iOS note
+
+The project is not yet compatible with iOS, but everything should be fine, as LibClang is actually compiled as a static library.
+
+Documentation
+-------------
+
+Documentation can be found at: http://xs-labs.com/en/projects/clang-kit/documentation/
+
+Project home
+------------
+
+Project home can be found at: http://xs-labs.com/en/projects/clang-kit/
+
+License
+-------
+
+ClangKit is released under the terms of the Boost Software License - Version 1.0.
+
+Repository Infos
+----------------
+
+    Owner:			Jean-David Gadina - XS-Labs
+    Web:			www.xs-labs.com
+    Blog:			www.noxeos.com
+    Twitter:		@macmade
+    GitHub:			github.com/macmade
+    LinkedIn:		ch.linkedin.com/in/macmade/
+    StackOverflow:	stackoverflow.com/users/182676/macmade
